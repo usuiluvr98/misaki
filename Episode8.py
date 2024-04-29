46ab30a2abc68324065fc1a45e437dd27a129a64
usuiluvr98 <168418155+usuiluvr98@users.noreply.github.com>
2024-04-29 20:00:37 +0530

    Add files via upload

--- Episode8.py
+++ Episode8.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-# Define the inputs and outputs for the AND gate
 inputs = np.array([[-1, -1, 1], [-1, 1, 1], [1, -1, 1], [1, 1, 1]])
 outputs = np.array([-1, -1, -1, 1])
 
@@ -13,7 +12,6 @@ for i in range(len(inputs)):
     activations = input_pattern
     y = output_pattern
     print("current weight matrix for ",i,"iteration = ",weights)
-    # Update weights and bias using Hebbian learning rule
     weights += np.dot(activations, y)
     bias += y
 
